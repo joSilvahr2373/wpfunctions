ccb214dbc6375e2926c534dafd054873c1fa5ee6
joSilvahr2373 <48606868+joSilvahr2373@users.noreply.github.com>
2019-04-04 17:36:10 -0300

    [CREATE]
    Chama de scripts js

---
+++ chama_scripts_js.php
@@ -0,0 +1,2 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
